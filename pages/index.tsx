e6eeeac8ac16d99b2f069c5927a8f376ef2a9000
Wevison Ramalho Silva <wevisonramalho@gmail.com>
2022-07-28 20:33:34 -0300

    [ FEATURE ] Adding meta tags.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,25 @@
 import type { NextPage } from "next";
+import Head from "next/head";
 
 const Home: NextPage = () => {
-  return <h1>Hello World!</h1>;
+  return (
+    <>
+      <Head>
+        <title>Disney Cards</title>
+        <meta
+          name="description"
+          content="Disney Cards! Conhecendo alguns dos personagens mais amados pelos fãs da disney."
+        />
+        <meta property="og:title" content="Disney Cards" />
+        <meta property="og:type" content="article" />
+        <meta property="og:image" content="/images/icon-castle.png" />
+        <meta property="og:url" content="https://disney-cards.vercel.app" />
+        <link rel="icon" href="/images/favicon.png" />
+      </Head>
+
+      <main></main>
+    </>
+  );
 };
 
 export default Home;
